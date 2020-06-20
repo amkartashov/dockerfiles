a2ef8efe9cd6661e959e7067a0d8988c0990ce3a
Andrey Kartashov <gorilych@gmail.com>
2020-06-20 17:54:07 +0700

    sshct: security fix: don't store SSHD keys in docker image

--- sshct/entrypoint.sh
+++ sshct/entrypoint.sh
@@ -30,5 +30,32 @@ fi
 # Run init script in background 
 tmux new-session -d -s init '/bin/init.sh'
 
+# create sshd keys, copied from postinstall script
+create_key() {
+    msg="$1"
+    shift
+    file="$1"
+    shift
+
+    if [ ! -f "$file" ] ; then
+        echo -n $msg
+        ssh-keygen -q -f "$file" -N '' "$@"
+        echo
+        if which restorecon >/dev/null 2>&1; then
+            restorecon "$file" "$file.pub"
+        fi
+        ssh-keygen -l -f "$file.pub"
+    fi
+}
+
+mkdir -p /home/.sshkeys
+
+create_key "Creating SSH2 RSA key; this may take some time ..." \
+  /home/.sshkeys/ssh_host_rsa_key -t rsa
+create_key "Creating SSH2 ECDSA key; this may take some time ..." \
+  /home/.sshkeys/ssh_host_ecdsa_key -t ecdsa
+create_key "Creating SSH2 ED25519 key; this may take some time ..." \
+  /home/.sshkeys/ssh_host_ed25519_key -t ed25519
+
 # start ssh daemon
 exec /usr/sbin/sshd -Def /etc/ssh/sshd_config
