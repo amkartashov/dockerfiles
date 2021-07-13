d561fd38178db0422e553863e46b5dac210c6aff
Andrey Kartashov <gorilych@gmail.com>
2021-07-14 02:55:41 +0700

    ansible(v0.2.0): install required roles from galaxy on run

--- ansible/entrypoint.sh
+++ ansible/entrypoint.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
 if [ -f requirements.txt ]; then
+  echo == Installing required python modules
   pip3 --quiet install --requirement requirements.txt
+fi
+
+if [ -f requirements.yml ]; then
+  echo == Installing required roles from ansible galaxy
+  ansible-galaxy install --role-file requirements.yml
 fi
 
 if [ $# -eq 0 ]; then
   echo No commands passed, running shell
   exec bash
 fi
+
+echo == Running command: "$@"
 
 exec "$@"
